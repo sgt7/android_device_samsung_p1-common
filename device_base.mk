909049433d71f147a26edee0f5308aeb8fa553b0
jt1134 <jt1134@gmail.com>
2012-04-15 01:59:44 -0500

    Revert "set device specific BT & WLAN MAC address on boot"
    This breaks wifi on p1c
    
    This reverts commit b80d9ed3a72b5fd2646ee38a1dd3a5619d941c00.

--- device_base.mk
+++ device_base.mk
@@ -74,14 +74,10 @@ PRODUCT_PACKAGES += \
 	hwcomposer.s5pc110 \
 	libstagefrighthw
 
-# tvout
+# Device-specific packages
 PRODUCT_PACKAGES += \
 	P1Parts \
 	tvouthack
-
-# script to set bluetooth and wlan MAC addresses
-PRODUCT_COPY_FILES += \
-        device/samsung/p1-common/prebuilt/bin/set-macaddr:system/vendor/bin/set-macaddr
 
 # Touchscreen
 PRODUCT_COPY_FILES += \
