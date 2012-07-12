20ef0591879a6cc61cea977721a03f165c950ee9
Chirayu Desai <chirayudesai1@gmail.com>
2012-07-12 19:21:46 +0530

    derp
    Change-Id: I7156bf424ad04a7fe5a8379d61fc07b6f7ca2b62

--- device_base.mk
+++ device_base.mk
@@ -28,8 +28,8 @@ PRODUCT_COPY_FILES := \
 # Init files
 PRODUCT_COPY_FILES += \
 	device/samsung/p1-common/init.p1-common.rc:root/init.p1-common.rc \
-	device/samsung/p1-common/init.p1usb.rc:root/init.p1usb.rc \
-	device/samsung/p1-common/init.p1usb.rc:recovery/root/init.p1usb.rc \
+	device/samsung/p1-common/init.p1.usb.rc:root/init.p1.usb.rc \
+	device/samsung/p1-common/init.p1.usb.rc:recovery/root/init.p1.usb.rc \
 	device/samsung/p1-common/lpm.rc:root/lpm.rc
 
 # Prebuilt kl keymaps
