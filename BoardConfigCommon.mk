1c2c8d2f68e3fa7f9fb4d34d287944bb7c5e7b44
Chirayu Desai <cdesai@cyanogenmod.org>
2012-11-24 12:02:34 +0530

    mr1 bringup
    Change-Id: I7ffe2739b9aa88a4c544a29beeb29c6f6f17cbaa
    
    Conflicts:
    	overlay/frameworks/base/core/res/res/values/config.xml

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -24,6 +24,7 @@ BOARD_USES_GENERIC_AUDIO := false
 BOARD_USES_LIBSECRIL_STUB := true
 
 # ARMv7-A Cortex-A8 architecture
+TARGET_ARCH := arm
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
 TARGET_ARCH_VARIANT := armv7-a-neon
@@ -44,6 +45,8 @@ TARGET_BOARD_SAMSUNG_P1 := true
 # Bluetooth
 BOARD_HAVE_BLUETOOTH := true
 BOARD_HAVE_BLUETOOTH_BCM := true
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/samsung/p1-common/bluetooth
+BOARD_BLUEDROID_VENDOR_CONF := device/samsung/p1-common/prebuilt/libbt_vndcfg.txt
 
 # WiFi related defines
 BOARD_WPA_SUPPLICANT_DRIVER := NL80211
