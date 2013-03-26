a9734b0b6e37e0a31e920304944fa38e8fb65b34
jt1134 <jt1134@gmail.com>
2013-03-25 19:28:54 -0500

    p1-common: misc updates
    - update BT permissions and include vendor conf
    - use common recovery.rc
    - opt-in to new bionic optimizations
    - remove some dead/useless code
    
    Change-Id: I3c7edbaa0fadf1aca3e31150bf899e4106c3d95c

--- libaudio/Android.mk
+++ libaudio/Android.mk
@@ -23,15 +23,3 @@ LOCAL_C_INCLUDES += \
 	$(call include-path-for, audio-utils)
 
 include $(BUILD_SHARED_LIBRARY)
-
-include $(CLEAR_VARS)
-
-LOCAL_SRC_FILES := AudioPolicyManager.cpp
-LOCAL_SHARED_LIBRARIES := libcutils libutils
-LOCAL_STATIC_LIBRARIES := libmedia_helper
-LOCAL_WHOLE_STATIC_LIBRARIES := libaudiopolicy_legacy
-LOCAL_MODULE := audio_policy.s5pc110
-LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
-LOCAL_MODULE_TAGS := optional
-
-include $(BUILD_SHARED_LIBRARY)
